27c80b88c89070263dc67480946fa46f5ef2b1dd
Solange Minossi <Solange Minossi@gmail.com>
2022-08-15 21:45:14 -0300

    Rotas, controllers, model, nodemon

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # petshop
 Projeto feito em aula com início de um código;
-Iniciando a arvore no VScode.
+
---
+++ controllers/PetsController.js
@@ -0,0 +1,10 @@
+const PetModel = require('../models/PetModel');
+
+const PetsController = {
+    index: (req, res) => {
+        const pets = PetModel.obterInformacoes();
+        res.render('pets', {pets: pets});
+}
+}
+
+module.exports = PetsController;
